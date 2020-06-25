f9f9f26de3caf059b6a291e5dbe1ead7a03688a7
grmanjula <63128282+grmanjula@users.noreply.github.com>
2020-06-24 23:31:57 -0700

    Rendering Event details

--- EventCatalogAPI/Controllers/CatalogController.cs
+++ EventCatalogAPI/Controllers/CatalogController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EventCatalogAPI.Data;
+using EventCatalogAPI.Domain;
+using EventCatalogAPI.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EventCatalogAPI.Controllers
 {
@@ -14,10 +17,12 @@ namespace EventCatalogAPI.Controllers
     public class CatalogController : ControllerBase
     {
         private readonly EventContext _context;
+        private readonly IConfiguration _config;
 
-        public CatalogController(EventContext context)
+        public CatalogController(EventContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
 
         }
         //To verify in postman https://localhost:44315/api/Catalog\items
@@ -28,13 +33,35 @@ namespace EventCatalogAPI.Controllers
            [FromQuery] int pageIndex =0,
             [FromQuery] int pageSize =6)
         {
+            var itemsCount = await _context.EventItems.LongCountAsync();
             var items = await   _context.EventItems
             .OrderBy(c => c.Name)
             .Skip(pageIndex * pageSize)
             .Take(pageSize)
             .ToListAsync();
 
-            return Ok(items);
+            items = ChangeImageUrl(items);
+
+            // items = ChangeImageUrl
+
+            var model = new PaginatedItemsViewModel<EventItem>
+            {
+                PageIndex = pageIndex,
+                PageSize = items.Count,
+                Data = items,
+                Count = itemsCount
+            };
+            return Ok(model);
+        }
+
+        private List<EventItem> ChangeImageUrl(List<EventItem> items)
+        {
+            items.ForEach(item => 
+            item.ImageUrl = item.ImageUrl.Replace
+            ("http://externaleventbaseurltobeplaced", 
+            _config["ExternalCatalogBaseUrl"]));
+
+            return items;
         }
     }
 }
---
+++ EventCatalogAPI/ViewModels/PaginatedItemsVieeModel.cs
@@ -0,0 +1,19 @@
+﻿using EventCatalogAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.ViewModels
+{
+    public class PaginatedItemsViewModel<T>
+    {
+
+        public int  PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public long  Count { get; set; }
+
+        public IEnumerable<T> Data { get; set; }
+    }
+}
+
